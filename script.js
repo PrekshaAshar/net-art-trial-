8380e47e636d55145c793ab6809290534da6ea78
PrekshaAshar <2022preksha.j@vidyashilp.edu.in>
2025-01-27 16:51:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-$('.book')
-  .on('click', '.active', nextPage)
-  .on('click', '.flipped', prevPage);
+var currentPage = 0;
 
-$('.book').hammer().on('swipeleft', nextPage);
-$('.book').hammer().on('swiperight', prevPage);
+$('.book')
+.on('click', '.active', nextPage)
+.on('click', '.flipped', prevPage);
+
+$('.book').hammer().on("swipeleft", nextPage);
+$('.book').hammer().on("swiperight", prevPage);
 
 function prevPage() {
-  $('.flipped').last().removeClass('flipped').addClass('active')
-    .siblings('.page').removeClass('active');
+  
+  $('.flipped')
+    .last()
+    .removeClass('flipped')
+    .addClass('active')
+    .siblings('.page')
+    .removeClass('active');
 }
 function nextPage() {
-  $('.active').removeClass('active').addClass('flipped')
-    .next('.page').addClass('active').siblings();
+  
+  $('.active')
+    .removeClass('active')
+    .addClass('flipped')
+    .next('.page')
+    .addClass('active')
+    .siblings();
+    
+    
 }
-
 
